5d92b2941446985f8b98821f2c6352af96401606
cisebaba <cisebabatasi@gmail.com>
2022-04-01 10:42:27 -0700

    Final submission

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from tasks.views import TaskListView, TaskUpdateView
+from tasks.views import TaskListView, TaskUpdateView, TaskCreateView
 
 urlpatterns = [
     path("mine/", TaskListView.as_view(), name="show_my_tasks"),
     path("<int:pk>/complete/", TaskUpdateView.as_view(), name="complete_task"),
+    path("create/", TaskCreateView.as_view(), name="create_task"),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic.list import ListView
-from django.views.generic.edit import UpdateView
+from django.views.generic.edit import UpdateView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from tasks.models import Task
 
@@ -19,3 +19,12 @@ class TaskUpdateView(LoginRequiredMixin, UpdateView):
     model = Task
     fields = ["is_completed"]
     success_url = reverse_lazy("show_my_tasks")
+
+
+class TaskCreateView(LoginRequiredMixin, CreateView):
+    model = Task
+    template_name = "tasks/create.html"
+    fields = ["name", "start_date", "due_date", "project", "assignee"]
+
+    def get_success_url(self):
+        return reverse_lazy("show_project", args=[self.object.id])
