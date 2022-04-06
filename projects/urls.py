3c80bab52d6f0a584576dcd168a512ba69570372
cisebaba <cisebabatasi@gmail.com>
2022-04-06 11:39:14 -0700

    search feature added

--- projects/urls.py
+++ projects/urls.py
@@ -3,10 +3,12 @@ from projects.views import (
     ProjectListView,
     ProjectDetailView,
     ProjectCreateView,
+    search_project,
 )
 
 urlpatterns = [
     path("", ProjectListView.as_view(), name="list_projects"),
     path("<int:pk>/", ProjectDetailView.as_view(), name="show_project"),
     path("create/", ProjectCreateView.as_view(), name="create_project"),
+    path("search_project/", search_project, name="search_project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,8 @@
 from django.urls import reverse_lazy
+from django.shortcuts import render
 from projects.models import Project
+from itertools import chain
+from tasks.models import Task
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
@@ -31,3 +34,20 @@ class ProjectCreateView(LoginRequiredMixin, CreateView):
 
     def get_success_url(self):
         return reverse_lazy("show_project", args=[self.object.id])
+
+
+def search_project(request):
+    """search function"""
+    if request.method == "POST":
+        query_name = request.POST.get("name", None)
+        if query_name:
+            projects = Project.objects.filter(name__contains=query_name)
+            tasks = Task.objects.filter(name__contains=query_name)
+            results = chain(projects, tasks)
+            return render(
+                request,
+                "projects/search_project.html",
+                {"results": results},
+            )
+
+    return render(request, "projects/search_project.html")
